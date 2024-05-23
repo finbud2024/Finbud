c377a0f6701e7b6fc531843039cd15eeb1ee139c
lollichilli <nguyth02@luther.edu>
2024-05-22 22:22:57 -0500

    Added chat thread and restyle the page

--- src/components/ChatHeader.vue
+++ src/components/ChatHeader.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="chat-header">
     <h1>FINBUD - FINANCIAL BUDDY</h1>
-    <h2>Thread ID: {{ threadId }}</h2> <!-- Display the threadId here -->
   </div>
 </template>
 
@@ -9,9 +8,9 @@
 export default {
   props: {
     threadId: {
-      type: [String, Number], // Accept both string and number types
-      default: '' // Default to an empty string if not provided
-    },
+      type: [String, Number],
+      default: ''
+    }
   },
   mounted() {
     console.log('ChatHeader mounted with thread ID:', this.threadId);
@@ -26,15 +25,13 @@ export default {
 
 <style scoped>
 .chat-header {
+  background-image: linear-gradient(to right, #3541c9, #3340b2);
+  color: #ecf0f1;
+  padding: 20px;
   text-align: center;
-  font-size: 1.5rem;
-  font-family: 'Helvetica Neue', sans-serif;
-  background-image: linear-gradient(to right, #4444c5, #2348db);
-  color: white;
-  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
-  border-bottom: 3px solid #1e81b0;
-  padding: 20px 0;
-  transition: background-image 0.5s ease-in-out, box-shadow 0.5s ease-in-out;
+  font-size: 1.5em;
+  font-weight: bold;
+  border-bottom: 2px solid #2980b9;
 }
 
 .chat-header:hover {
--- src/views/ChatView.vue
+++ src/views/ChatView.vue
@@ -1,42 +1,40 @@
 <template>
   <div class="home-container">
-    <SideBar /> <!-- Include the sidebar here -->
+    <SideBar
+      :threads="threads"
+      @add-thread="addThread"
+      @edit-thread="editThread"
+      @save-thread-name="saveThreadName"
+      @cancel-edit="cancelEdit"
+      @select-thread="selectThread"
+    />
     <div class="chat-container">
-      <ChatHeader :threadId="currentThread" />
+      <ChatHeader :threadId="currentThread.id" />
       <ChatFrame>
-        <MessageComponent v-for="(message, index) in messages" :key="index"
-          :is-user="message.isUser" :text="message.text" :typing="message.typing"
-          :timestamp="message.timestamp" :username="message.isUser ? 'Tri Bui' : 'FinBud Bot'"
-          :avatar-src="message.isUser ? userAvatar : botAvatar" />
+        <MessageComponent
+          v-for="(message, index) in messages"
+          :key="index"
+          :is-user="message.isUser"
+          :text="message.text"
+          :typing="message.typing"
+          :timestamp="message.timestamp"
+          :username="message.isUser ? 'Tri Bui' : 'FinBud Bot'"
+          :avatar-src="message.isUser ? userAvatar : botAvatar"
+        />
       </ChatFrame>
       <UserInput @send-message="sendMessage" @clear-message="clearMessage" />
     </div>
   </div>
 </template>
-<style>
-.home-container {
-  display: flex; /* Changes the flex-direction to row by default */
-  width: 100%; /* Full width of the viewport */
-  height: 100vh; /* Full height of the viewport */
-}
-
-.chat-container {
-  flex-grow: 1; /* Takes up the remaining space */
-  display: flex;
-  flex-direction: column;
-}
-</style>
-
 
 <script>
 import ChatHeader from '../components/ChatHeader.vue';
-import ChatFrame from '../components/ChatFrame.vue'
+import ChatFrame from '../components/ChatFrame.vue';
 import MessageComponent from '../components/MessageComponent.vue';
 import UserInput from '../components/UserInput.vue';
 import { fetchStockPrice } from '@/services/stockServices';
 import { gptAPICall } from '@/services/gptServices';
 import SideBar from '../components/SideBar.vue';
-
 
 export default {
   name: 'ChatView',
@@ -54,54 +52,32 @@ export default {
       messages: [],
       userAvatar: require('@/assets/tri.jpeg'),
       botAvatar: require('@/assets/bot.png'),
-      currentThread: {}, // The current selected thread
+      currentThread: {},
+      threads: []
     };
   },
-
   watch: {
     threadId: {
       immediate: true,
       handler(newThreadId) {
         console.log('Received new thread ID:', newThreadId);
         if (newThreadId != null) {
-          console.log("Hello");
           this.updateCurrentThread(newThreadId);
         }
       }
-    },
-  },
-  computed: {
-    currentChatWindow() {
-      // Dynamic component name based on the selected thread or default chat window
-      return this.currentThread.id ? 'ChatWindow' : 'DefaultChatWindow';
     },
   },
   methods: {
     clearMessage() {
       this.newMessage = ''
     },
-    // sendMessage(newMessage) {
-    //   console.log("start sending this.newMessage: ", newMessage)
-    //   this.messages.push({
-    //     text: newMessage.trim(),
-    //     isUser: true,
-    //     typing: true,
-    //     timestamp: new Date().toLocaleTimeString()
-    //   });
-    //   // Simulate a delay for typing effect, then set typing to false
-    //   console.log("Updated messages array: ", this.messages);
-
-    //   this.newMessage = '';
-    // },
-
     updateCurrentThread(newThreadId) {
       console.log("Update current thread function triggered: ", newThreadId)
-      // const thread = this.threads.find(thread => thread.id.toString() === newThreadId);
-      const thread = newThreadId.toString();
+      const thread = this.threads.find(thread => thread.id.toString() === newThreadId);
       console.log(thread)
       if (thread) {
         this.currentThread = thread;
-        // this.messages = thread.messages || [];
+        this.messages = thread.messages || [];
       } else {
         console.error('Thread with ID', newThreadId, 'not found');
         this.currentThread = {};
@@ -109,9 +85,25 @@ export default {
       }
       console.log("Current thread: ", this.currentThread)
     },
+    addThread(newThread) {
+      newThread.id = this.threads.length + 1;
+      this.threads.push(newThread);
+      console.log("Added new thread: ", newThread);
+    },
+    editThread(index) {
+      this.threads[index].editing = true;
+    },
+    saveThreadName({ newName, index }) {
+      this.threads[index].name = newName;
+      this.threads[index].editing = false;
+    },
+    cancelEdit(index) {
+      this.threads[index].editing = false;
+    },
+    selectThread(index) {
+      this.updateCurrentThread(this.threads[index].id.toString());
+    },
     async sendMessage(newMessage) {
-      // if (!this.newMessage.trim()) return;
-
       console.log("start sending message in send message..")
 
       this.messages.push({
@@ -120,25 +112,16 @@ export default {
         typing: true,
         timestamp: new Date().toLocaleTimeString()
       });
-      // Simulate a delay for typing effect, then set typing to false
 
       this.newMessage = '';
 
       const stockCode = this.extractStockCode(this.messages[this.messages.length - 1].text);
-      if (stockCode != '') {
+      if (stockCode.length > 0) {
         try {
           const price = await fetchStockPrice(stockCode);
           const timeStamp = new Date().toLocaleTimeString();
-          console.log("This is time stamp when ask about stock price: ", timeStamp)
           let responseText = `Giá cổ phiếu ${stockCode} hiện giờ là $${price}, tính theo mốc thời gian ${timeStamp} `;
-          // if (shares !== null) {
-          //   // If both stock code and shares are provided, calculate total value
-          //   const totalValue = (Number(price) * shares).toFixed(2);
-          //   responseText = `Bạn có ${shares} cổ phiếu ${stockCode}, với tổng trị giá là $${totalValue}.`;
-          //   this.messages.push({ text: `Bạn có ${shares} cổ phiếu ${stockCode}, với tổng trị giá là $${totalValue}.`, isUser: false, });
-          // }
           this.addTypingResponse(responseText, false);
-          // Send a fixed message to the GPT API asking for analysis about the company
           const analysisQuestion = `Phân tích các số liệu sau từ công ty Tesla Chỉ Số Nâng CaoTên	Công ty	Ngành
       Tỉ số P/E TTM	39.31	18.18
   Giá trên doanh thu TTM	5.66	3.93
@@ -146,7 +129,6 @@ export default {
   Giá và dòng tiền mặt tự do TTM	388.36	33.6
   Giá trên giá ghi sổ sách MRQ	8.34	2.67
   Giá trên sổ sách hữu hình MRQ	8.39	0.64
-        
   Khả năng sinh lợi: TTM đối với Biên Lợi Trung Bình 5 Năm TTM (%)  TB 5 năm (%)
   Lãi gộp
   Lãi gộp kinh doanh
@@ -161,7 +143,6 @@ export default {
   Hệ số biên lợi nhuận ròng
        TTM (%)	14.37%
        TB 5 năm (%)	7.59%
-   
   Lãi gộp TTM	17.78%	17.76
   Lãi Gộp 5YA	21.49%	14.78
   Lãi gộp kinh doanh TTM	7.81%	-8.16
@@ -170,7 +151,6 @@ export default {
   Tỷ lệ lời trước thuế 5YA	7.96%	-2,230.78
   Hệ số biên lợi nhuận ròng TTM	14.37%	-11.97
   Hệ số biên lợi nhuận ròng 5YA	7.59%	-2,209.64
-        
   Doanh thu/Cổ phần TTM	29.75	11,209.08
   EPS cơ bản ANN	4.73	637.24
   EPS pha loãng ANN	4.3	637.15
@@ -188,23 +168,16 @@ export default {
           console.error('Error:', error);
           this.messages.push({ text: `Error fetching data for ${stockCode}.`, isUser: false, timestamp: new Date().toLocaleTimeString() });
         }
-      }
-      else {
+      } else {
         setTimeout(
           () => {
             this.addTypingResponse('Tôi là FinBud, trợ lý AI trong lĩnh vực tài chính của bạn, phát triển bởi Bui Dinh Tri. Tôi được cấu hình đặc biệt trên nền tảng GPT-3.5 Turbo, với mục tiêu cung cấp thông tin sâu hơn và chuyên môn về tài chính và thị trường cổ phiếu.', false);
           }, 300
         )
       }
-
-
-
-
-
     },
 
     addTypingResponse(text, isUser) {
-      // Add a typing message first
       const typingMessage = {
         text: text,
         isUser: isUser,
@@ -217,81 +190,39 @@ export default {
       setTimeout(() => {
         typingMessage.text = text;
         typingMessage.typing = false;
-        this.$forceUpdate(); // Force update to show changes
-      }, 2000); // simulate typing delay
+        this.$forceUpdate();
+      }, 2000);
     },
 
     extractStockCode(message) {
       const pattern = /\b[A-Z]{3,5}\b/g;
       const matches = message.match(pattern) || [];
-      console.log("Ham extract: ", matches)
       return matches;
     },
 
     extractSharesAndCode(message) {
-      // Regex to find stock codes: assumes codes are upper-case letters optionally followed by numbers
       const stockCodePattern = /\b[A-Z]{1,5}\b/g;
-      // Regex to find numbers: looks for numbers that may have commas and come after the word 'shares'
       const sharesPattern = /(\b\d{1,3}(,\d{3})*\b)(?=\s*shares)/gi;
 
-      const stockCodes = message.match(stockCodePattern);
-      const sharesMatches = message.match(sharesPattern);
+      const stockCodes = message.match(stockCodePattern) || [];
+      const shares = message.match(sharesPattern) || [];
 
-      let shares;
-      if (sharesMatches && sharesMatches.length > 0) {
-        // Remove commas and convert to integer
-        shares = parseInt(sharesMatches[0].replace(/,/g, ''), 10);
-      }
-
-      // We return the first stock code found and the number of shares, if any
-      return {
-        stockCode: stockCodes ? stockCodes[0] : null, // Just taking the first match for simplicity
-        shares: shares || null
-      };
-    },
-
-    async calculateTotalValue(stockCode, shares) {
-      if (!stockCode || !shares) {
-        throw new Error("Stock code or number of shares missing.");
-      }
-      try {
-        const price = await fetchStockPrice(stockCode);
-        // Convert price to a number and calculate total value
-        const totalValue = (Number(price) * shares).toFixed(2);
-        return totalValue;
-      } catch (error) {
-        console.error('Error calculating total value:', error);
-        throw error; // Re-throw the error to handle it in the calling function
-      }
-    },
-
-    async fakeResponse(message) {
-      // Simulate fake response from the backend API
-      return "This is a fake response to the message: " + message;
-    },
-
-
-    mounted() {
-      // Update the current time every second
-      setInterval(() => {
-        this.currentTime = new Date().toLocaleTimeString();
-      }, 500);
+      return { stockCodes, shares };
     }
   }
 }
 </script>
 
-<style>
+
+<style scoped>
 .home-container {
-  width: 100%;
-  /* Ensures the container fills the viewport width */
-  box-sizing: border-box;
-  /* Includes padding and border in the element's total width */
+  display: flex;
+  height: 100vh;
 }
 
 .chat-container {
   display: flex;
   flex-direction: column;
-  height: 100%;
+  flex: 1;
 }
 </style>
