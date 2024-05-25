107b97f6bae6e1a930bba0bc931ebdc760ce998c
Tri <hungnd@newwave.vn>
2024-05-24 19:56:01 -0500

    update

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
+const webpack = require('webpack');
+
 module.exports = {
   transpileDependencies: [],
   devServer: {
@@ -7,5 +9,25 @@ module.exports = {
         changeOrigin: true,
       },
     },
+  },
+  configureWebpack: {
+    resolve: {
+      fallback: {
+        crypto: require.resolve('crypto-browserify'),
+        stream: require.resolve('stream-browserify'),
+        assert: require.resolve('assert/'),
+        http: require.resolve('stream-http'),
+        https: require.resolve('https-browserify'),
+        os: require.resolve('os-browserify/browser'),
+        url: require.resolve('url/'),
+        path: require.resolve('path-browserify')
+      }
+    },
+    plugins: [
+      new webpack.ProvidePlugin({
+        process: 'process/browser',
+        Buffer: ['buffer', 'Buffer']
+      })
+    ]
   }
-}
+};
