b2b47fa43a7f45a19369839811a12abca98ef0ff
Tri <hungnd@newwave.vn>
2024-05-25 20:45:52 -0500

    update netlify

---
+++ backend/node_modules /vue-codemod/dist/transformations/remove-vue-set-and-delete.d.ts
@@ -0,0 +1,6 @@
+import type { ASTTransformation } from '../src/wrapAstTransformation';
+import type * as N from 'jscodeshift';
+export declare const transformAST: ASTTransformation;
+declare const _default: N.Transform;
+export default _default;
+export declare const parser = "babylon";
