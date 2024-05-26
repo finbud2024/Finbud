41e669046f845ce1aac9212a89e9803186ba8815
Tri <hungnd@newwave.vn>
2024-05-26 11:01:45 -0500

    update

--- backend/node_modules /@vue/cli/lib/util/registries.js
+++
@@ -1,8 +0,0 @@
-const registries = {
-  npm: 'https://registry.npmjs.org',
-  yarn: 'https://registry.yarnpkg.com',
-  taobao: 'https://registry.npmmirror.com',
-  pnpm: 'https://registry.npmjs.org'
-}
-
-module.exports = registries
