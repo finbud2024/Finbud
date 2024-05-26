5f8bedf5d7c6e65564526843635b4ffb598ebd61
Tri <hungnd@newwave.vn>
2024-05-25 20:45:52 -0500

    update netlify

---
+++ backend/node_modules /fkill/index.d.ts
@@ -0,0 +1,67 @@
+declare namespace fkill {
+	interface Options {
+		/**
+		Force kill the processes.
+
+		@default false
+		*/
+		readonly force?: boolean;
+
+		/**
+		Force kill processes that did not exit within the given number of milliseconds.
+
+		@default undefined
+		*/
+		readonly forceAfterTimeout?: number;
+
+		/**
+		Kill all child processes along with the parent process. _(Windows only)_
+
+		@default true
+		*/
+		readonly tree?: boolean;
+
+		/**
+		Ignore capitalization when killing a process.
+
+		Note that the case is always ignored on Windows.
+
+		@default false
+		*/
+		readonly ignoreCase?: boolean;
+
+		/**
+		Suppress all error messages. For example: `Process doesn't exist`.
+
+		@default false
+		*/
+		readonly silent?: boolean;
+	}
+}
+
+/**
+Fabulously kill processes. Cross-platform.
+
+@param input - One or more process IDs/names/ports to kill. To kill a port, prefix it with a colon. For example: `:8080`.
+
+@example
+```
+import fkill = require('fkill');
+
+(async () => {
+	await fkill(1337);
+	console.log('Killed process');
+})();
+
+fkill('Safari');
+fkill(':8080');
+
+fkill([1337, 'Safari', ':8080']);
+```
+*/
+declare function fkill(
+	input: number | string | ReadonlyArray<string | number>,
+	options?: fkill.Options
+): Promise<void>;
+
+export = fkill;
