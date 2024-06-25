48d77878582ecdfd65e7830cbbe5c336c8d8aff8
Dung Le <DungLe2911@gmail.com>
2024-06-25 00:47:23 -0400

    Fixing deployment

--- backend/functions/server.js
+++ backend/functions/server.js
@@ -5,15 +5,17 @@ import mongoose from 'mongoose';
 import serverless from 'serverless-http';
 import passportConfig from '../Passport/config.js';
 //routes for processing users request
+import dotenv from 'dotenv';
 import threadRoute from '../Endpoints/threadRoute.js';
 import userRoute from '../Endpoints/userRoute.js';
 import newsRoute from '../Endpoints/newsRoute.js';
 import chatRoute from '../Endpoints/chatRoute.js';
-import serverless from 'serverless-http';
+import authRoute from '../Endpoints/authRoute.js';
 
 // Load environment variables from .env
 const mongoURI = process.env.MONGO_URI;
 const app = express();
+
 
 if (!mongoURI) {
   console.error('MONGO_URI is not defined in the environment variables');
@@ -28,12 +30,13 @@ mongoose.connect(mongoURI)
   process.exit(1);
 });
 
+passportConfig(app)
+
+
 // Set up Express middlewares
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(cors())
-
-passportConfig(app)
 
 app.post('/analyzeRisk', async (req, res) => {
   console.log("Request from server.js:", req.body);
@@ -55,6 +58,7 @@ app.use('/.netlify/functions/server', userRoute);
 app.use('/.netlify/functions/server', threadRoute)
 app.use('/.netlify/functions/server', newsRoute);
 app.use('/.netlify/functions/server', chatRoute);
+app.use('/.netlify/functions/server', authRoute);
 
 const handler = serverless(app);
 export { handler };
