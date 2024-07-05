fc73294fcb4b4155447ae71378ee6445fc6ae0b4
Huy Phung <huyphung3103@gmail.com>
2024-07-04 23:08:24 -0400

    transaction

---
+++ backend/Database Schema/Transaction.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+const transactionSchema = new mongoose.Schema({
+  description: { type: String, required: true },
+  amount: { type: Number, required: true },
+  balance: { type: Number, required: true }, // Add balance field
+  date: { type: Date, default: Date.now },
+  transaction: { type: String, required: true }
+});
+
+const Transaction = mongoose.model('Transaction', transactionSchema);
+
+export default Transaction;
