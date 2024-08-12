666809941c56ec7496cfe44aab0a1259eb7ad83f
Dung Le <lehoangdung29111998@gmail.com>
2024-08-11 22:00:05 -0400

    revert back on vue.config.js

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -6,27 +6,5 @@ export default {
             .use('csv-loader')
             .loader('csv-loader')
             .end();
-    },
-    devServer: {
-        proxy: {
-            '/api': {
-                target: 'https://api.search.brave.com',
-                changeOrigin: true,
-                pathRewrite: { '^/api': '' },
-                secure: false,
-                onProxyReq: (proxyReq, req, res) => {
-                    proxyReq.setHeader('Access-Control-Allow-Origin', '*');
-                }
-            },
-            '/serper': {
-                target: 'https://google.serper.dev',
-                changeOrigin: true,
-                pathRewrite: { '^/serper': '' },
-                secure: false,
-                onProxyReq: (proxyReq, req, res) => {
-                    proxyReq.setHeader('Access-Control-Allow-Origin', '*');
-                }
-            }
-        }
     }
 };
