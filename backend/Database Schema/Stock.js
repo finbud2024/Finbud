2af94da3ffe0e331dd40cfc1050e3042a86c8165
Huy Phung <huyphung3103@gmail.com>
2024-08-29 22:39:34 -0400

    add stock seeding

--- backend/Database Schema/Stock.js
+++ backend/Database Schema/Stock.js
@@ -1,15 +1,22 @@
 import mongoose from "mongoose";
 
 const StockSchema = new mongoose.Schema({
-    symbol: { type: String, required: true },
-    open: { type: Number, required: true },
-    high: { type: Number},
-    low : {type: Number},
-    close: { type: Number, },
-    change: { type: Number},
-    volume: { type: Number},
-    date: { type: Date, required: true}
+    symbol: { 
+        type: String, 
+        required: true,
+        unique: true
+    },
+    open: { type: [Number], },
+    high: {  type: [Number], },
+    low : { type: [Number], },
+    close: { type: [Number], },
+    change: { type: [Number], },
+    volume: { type: [Number], },
+    date: { 
+        type: [Date], 
+        required: true
+    }
 })
 
-const StockPrice = mongoose.model('StockPrice', StockSchema);
-export default StockPrice;
+const Stock = mongoose.model('Stock', StockSchema);
+export default Stock;
