2c0ba8c306adefd948cdf05dd9bf7d12369259e9
hieuapg <146419538+hieuapg@users.noreply.github.com>
2024-09-02 01:23:22 -0400

    Update Transaction.js

--- backend/Database Schema/Transaction.js
+++ backend/Database Schema/Transaction.js
@@ -2,10 +2,11 @@ import mongoose from 'mongoose';
 
 const transactionSchema = new mongoose.Schema({
   description: { type: String, required: true },
+  notes: { type: String, default: null },
   amount: { type: Number, required: true },
   balance: { type: Number, required: true }, // Add balance field
-  date: { type: Date, default: Date.now },
-  transaction: { type: String, required: true },
+  date: { type: Date, default: Date.now, required: true },
+  type: {type: String, required: true},
   userId:{
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
