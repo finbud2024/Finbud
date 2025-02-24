aed069f0621a497b22f54bdf0e73a56587c7af86
PierceLe <haichau.hvsg@gmail.com>
2025-02-24 15:19:20 +1100

    add: successfully calling trading view api

--- backend/Endpoints/stockRoute.js
+++ backend/Endpoints/stockRoute.js
@@ -1,8 +1,54 @@
 import express from 'express';
 import StockPrice from '../Database Schema/Stock.js';
 import validateRequest from '../utils/validateRequest.js';
+import axios from 'axios';
 
 const stockRoute = express.Router();
+
+stockRoute.get("/api/stocks", async (req, res) => {
+    try {
+        const response = await axios.post(
+            "https://scanner.tradingview.com/global/scan?label-product=markets-screener",
+            {
+                columns: [
+                    "name", "description", "logoid", "update_mode", "type", "typespecs",
+                    "country.tr", "country_code_fund", "market_cap_basic",
+                    "fundamental_currency_code", "close", "pricescale", "minmov",
+                    "fractional", "minmove2", "currency", "change", "relative_volume_10d_calc",
+                    "price_earnings_ttm", "earnings_per_share_diluted_ttm",
+                    "earnings_per_share_diluted_yoy_growth_ttm", "dividends_yield_current",
+                    "sector.tr", "market", "sector", "recommendation_mark"
+                ],
+                ignore_unknown_fields: false,
+                options: { lang: "vi" },
+                range: [0, 100],
+                sort: {
+                    sortBy: "market_cap_basic",
+                    sortOrder: "desc",
+                    nullsFirst: false
+                },
+                preset: "worlds_largest_companies"
+            },
+            {
+                headers: {
+                    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:135.0) Gecko/20100101 Firefox/135.0",
+                    "Accept": "application/json",
+                    "Accept-Language": "en-US,en;q=0.5",
+                    "Accept-Encoding": "gzip, deflate, br, zstd",
+                    "Referer": "https://vn.tradingview.com/",
+                    "Content-Type": "text/plain;charset=UTF-8",
+                    "Origin": "https://vn.tradingview.com",
+                    "Connection": "keep-alive"
+                }
+            }
+        );
+
+        res.json(response.data);
+    } catch (error) {
+        console.error("Error fetching data from TradingView:", error.message);
+        res.status(500).json({ error: "Failed to fetch data" });
+    }
+});
 
 stockRoute.post("/updateStockDB", validateRequest(StockPrice.schema), async (req, res) => {
     const symbol = req.body.symbol;
