305593f135b9d2cce2cd6f4b0ce79a2e46f544b1
Hachu04 <chuvietha04@gmail.com>
2025-04-10 22:17:15 -0400

    Test Cookie sameSite: isProduction

--- backend/Passport/config.js
+++ backend/Passport/config.js
@@ -39,7 +39,7 @@ const passportConfig = (app) => {
             maxAge: 1000 * 60 * 60 * 24,
             httpOnly: true,
             path: "/",
-            sameSite: isProduction || isNetlifyDev ? "none" : "lax",
+            sameSite: isProduction,
             secure: isProduction || isNetlifyDev
           }
         })
