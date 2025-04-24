cfb64e2ca38ca7c6eab06fd7168378272f213fa0
maggie-nguyen <mainguyenngoc513@gmail.com>
2025-04-24 11:43:54 +0900

    forecasting

--- backend/deleteTransactions.js
+++ backend/deleteTransactions.js
@@ -1,28 +1,28 @@
-// deleteTransactions.js
-import dotenv from 'dotenv';
+// // deleteTransactions.js
+// import dotenv from 'dotenv';
 
-import mongoose from 'mongoose';
-import Transaction from './Database Schema/Transaction.js'; 
+// import mongoose from 'mongoose';
+// import Transaction from './Database Schema/Transaction.js'; 
 
-dotenv.config();
+// dotenv.config();
 
-const MONGO_URI = process.env.MONGO_URI;
+// const MONGO_URI = process.env.MONGO_URI;
 
-if (!MONGO_URI) {
-  console.error('❌ MONGO_URI not found in .env file.');
-  process.exit(1);
-}
+// if (!MONGO_URI) {
+//   console.error('❌ MONGO_URI not found in .env file.');
+//   process.exit(1);
+// }
 
-async function deleteTransactions() {
-  try {
-    await mongoose.connect(MONGO_URI);
-    const result = await Transaction.deleteMany({ userId: '67d8f8f4d99e44814a2a64fe' });
-    console.log(`✅ Deleted ${result.deletedCount} transactions.`);
-  } catch (err) {
-    console.error('❌ Error deleting transactions:', err);
-  } finally {
-    await mongoose.disconnect();
-  }
-}
+// async function deleteTransactions() {
+//   try {
+//     await mongoose.connect(MONGO_URI);
+//     const result = await Transaction.deleteMany({ userId: '67d8f8f4d99e44814a2a64fe' });
+//     console.log(`✅ Deleted ${result.deletedCount} transactions.`);
+//   } catch (err) {
+//     console.error('❌ Error deleting transactions:', err);
+//   } finally {
+//     await mongoose.disconnect();
+//   }
+// }
 
-deleteTransactions();
+// deleteTransactions();
